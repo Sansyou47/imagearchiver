0e75996d04068067327a03d585901045a5dfa736
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2024-02-25 18:17:09 +0900

    デコレータへのメソッドの処理を変更

--- web/app/app.py
+++ web/app/app.py
@@ -5,9 +5,25 @@ app = Flask(__name__)
 
 app.register_blueprint(resize.resize)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        num = request.form.get("submit")
+        if num == "1":
+            resize.imgResize(155)
+        elif num == "2":
+            resize.imgResize(300)
+        elif num == "3":
+            resize.imgResize(600)
+        else:
+            res = request.form.get("text")
+            if res is None:
+                return redirect('/')
+            else:
+                resize.imgResize(int(res))
+        return redirect('/')
+    else:
+        return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
