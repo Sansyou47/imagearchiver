f1bde6950a6fc65b567f110851c07efdff5adcc2
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2024-02-26 17:47:00 +0900

    保存先の変数をモジュール化

--- web/app/function/resize.py
+++ web/app/function/resize.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template, request, Blueprint
 from PIL import Image
+from function import variable
 
 resize = Blueprint("resize", __name__)
 
-# オリジナル画像の保存先
-imgLocation_origin = 'images/origin/'
-# リサイズ画像の保存先
-imgLocation_resized = 'images/downsize/'
-
-def imgResize(weight, qlv):
-    img = Image.open(imgLocation_origin + 'test.jpg')
-    
+def imgResize(imgName, weight, qlv):
+    img = Image.open(variable.imgLocation_origin + imgName)
     # 画像のサイズを取得
     before = img.size
 
@@ -23,17 +18,21 @@ def imgResize(weight, qlv):
     # リサイズ処理
     img_resized = img.resize(after)
     
+    # メタデータの生成
     meta = meta_generate(before, after, qlv)
+    
+    imgName = imgName.split('.')[0]
 
     # リサイズ画像を保存（ファイル名にメタデータを付与）
-    img_resized.save(imgLocation_resized + 'test' + '(' + str(meta) + ')' + '.jpg', quality=qlv)
+    img_resized.save(variable.imgLocation_resized + imgName + str(meta) + '.jpg', quality=qlv)
 
+# メタデータ生成関数
 def meta_generate(src_before, src_after, qlv):
     if src_before[0] > src_after[0]:
-        dmeta = 'downsizing_'
+        dmeta = '_(downsizing_'
     else:
-        dmeta = 'enlargement_'
+        dmeta = '_(enlargement_'
     dmeta += 'resolution=' + str(src_after[0]) + 'x' + str(src_after[1]) + '_'
-    dmeta += 'quality=' + str(qlv)
+    dmeta += 'quality=' + str(qlv) + ')'
     
     return dmeta
