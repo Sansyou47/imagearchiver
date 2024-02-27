047c111d2118fa524b7a4c405763d52125231d81
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2024-02-28 01:47:41 +0900

    データベースへ登録する処理を実装

--- web/app/function/save.py
+++ web/app/function/save.py
@@ -1,18 +1,34 @@
 from flask import Flask, render_template, request, Blueprint
+from flaskext.mysql import MySQL
 from PIL import Image
 from function import variable
+import os
 
 save = Blueprint("save", __name__)
 
-# 画像保存関数
-def imgSave(img):
-    imageName = img.filename
-    img.save(variable.imgLocation_origin + imageName)
-    return imageName
+mysql = None
 
-# 対応した拡張子か判定する関数
-def extensionJudge(imgName):
-    if imgName.split('.')[-1] not in variable.supportedExtentionList:
+# 画像保存関数
+def imgSave(img, mysql):
+    imageName = img.filename
+    ext = extensionJudge(imageName)
+    if ext == False:
         return False
     else:
-        return True
+        img.save(variable.imgLocation_origin + imageName)
+        file_size = os.path.getsize(variable.imgLocation_origin + imageName)
+        cursor = mysql.get_db().cursor()
+        cursor.execute('INSERT INTO images(imageName, filePath, fileType, fileSize,userId) VALUES (%s, %s, %s, %s, %s)', (imageName, variable.imgLocation_origin, ext, file_size, 1))
+        cursor.connection.commit()
+        cursor.close()
+        return imageName
+
+# 対応した拡張子か判定する関数（対応していれば拡張子を返す）
+def extensionJudge(imgName):
+    ext = imgName.split('.')[-1]
+    if ext not in variable.supportedExtentionList:
+        return False
+    else:
+        if ext == 'jpeg':
+            ext = 'jpg'
+        return ext
