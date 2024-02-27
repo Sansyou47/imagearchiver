511d99c481d7885ca1911ba7fa4c84203b0e5f00
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2024-02-28 01:46:29 +0900

    ユーザーテーブルと画像テーブルの作成

--- db/data/init.sql
+++ db/data/init.sql
@@ -1,17 +1,20 @@
 drop table if exists users;
 create table users(
-    userId int primary key auto_increment,
-    userName varchar(255) not null,
-    email varchar(255),
-    password varchar(255) not null
+    userId      int primary key auto_increment,
+    userName    varchar(255) not null,
+    email       varchar(255),
+    password    varchar(255) not null
 );
 
 INSERT INTO users(userName, email, password) VALUES ('テストユーザー', 'test@localhost.jp', 'testuser');
 
-drop table if exists files;
-create table files(
-    fileId int primary key auto_increment,
-    fileName varchar(255) not null,
-    filePath varchar(255) not null,
-    userId int not null,
+drop table if exists images;
+create table images(
+    imageId     int primary key auto_increment,
+    imageName   varchar(255) not null,
+    filePath    varchar(255) not null,
+    fileType    varchar(255) not null,
+    fileSize    int not null,
+    importDate  datetime default current_timestamp,
+    userId      int not null
 );
