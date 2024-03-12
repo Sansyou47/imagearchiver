42b6709ad13302629e7816862177f26cae6f21b0
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2024-03-13 00:20:57 +0900

    フィボナッチ計算速度の可視化

--- web/app/app.py
+++ web/app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flaskext.mysql import MySQL
-from function import resize, save
-import ctypes, subprocess, os
+from function import resize, save, variable
+import ctypes, subprocess, os, time
 
-# libc = ctypes.cdll.LoadLibrary("./c/sample.so")
+libc = ctypes.cdll.LoadLibrary("./c/sample.so")
 
 app = Flask(__name__)
 
@@ -26,6 +26,8 @@ def index():
         num = request.form.get('preset')
         quality = request.form.get('quality')
         image = request.files.get('example')
+        do_encrypt = request.form.get('do_encrypt')
+        do_encrypt = int(do_encrypt)
         
         if num is None:
             return redirect('/')
@@ -54,11 +56,18 @@ def index():
             else:
                 res = int(res)
                 resize.imgResize(imageName, res, quality)
-        
-        # inputfile = '/app/test_raw.txt'
-        # outputfile = '/app/test_enc.txt'
-        
-        # libc.encrypt_image_file(inputfile.encode(), outputfile.encode())
+        if do_encrypt == 1:
+            # 暗号化するファイルのパス
+            inputfile = '/app/images/origin/' + imageName
+            # 暗号化後のファイルのパス
+            outputfile = '/app/' + variable.imgLocation_encrypted + imageName
+            variable.enc_file(inputfile.encode(), outputfile.encode())
+        elif do_encrypt == 2:
+            # 暗号化するファイルのパス
+            inputfile = '/app/images/origin/' + imageName
+            # 暗号化後のファイルのパス
+            outputfile = '/app/' + variable.imgLocation_encrypted + imageName
+            libc.encrypt_image_file(inputfile.encode(), outputfile.encode())
         
         return redirect('/')
     else:
@@ -68,15 +77,31 @@ def index():
 def fib():
     if request.method == 'POST':
         num = request.form.get('number')
+        count = request.form.get('count')
         preset = request.form.get('preset')
         if num is None:
             return redirect('/fib')
         num = int(num)
+        count = int(count)
+        total_time = 0
+        preset = int(preset)
         if preset == 1:
-            result = fib(num)
-        # else:
-        #     result = libc.fib(num)
-        return str(result)
+            for i in range(count):
+                start_time = time.time()
+                result = fibn(num)
+                end_time = time.time()
+                pre = 'Python'
+                total_time += end_time - start_time
+        else:
+            for i in range(count):
+                start_time = time.time()
+                result = libc.fib(num)
+                end_time = time.time()
+                pre = 'C'
+                total_time += end_time - start_time
+            
+        average_time = total_time / count * 1000  # ミリ秒単位に変換
+        return '計算結果：' + str(result) + ', ' + '実行回数：' + str(count) + ', 平均時間：' + format(average_time, '.4f') + 'ミリ秒, ' + '言語：' + pre  # 4桁まで表示
     else:
         return render_template('fib.html')
     
@@ -88,6 +113,11 @@ def comp():
 def compile():
     subprocess.run(['gcc', './c/sample.c', '-shared', '-o', './c/sample.so'])
     return redirect('/compile')
+
+def fibn(n):
+    if n <= 1:
+        return n
+    return fibn(n - 1) + fibn(n - 2)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
