413b1c152f57234e18361c40d2b6bf0444f7aa72
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2024-03-14 16:21:48 +0900

    ユーザーパスワードで複合化

--- web/app/function/variable.py
+++ web/app/function/variable.py
@@ -17,11 +17,11 @@ def xor_enc_dec(input, output, length, secret_key):
         if count == len(secret_key):
             count = 0
         
-def enc_file(inputfile, outputfile, key):
+def enc_file(inputfile, outputfile, secretKey):
     with open(inputfile, 'rb') as f:
         data = f.read()
     length = len(data)
     data = bytearray(data)
-    xor_enc_dec(data, data, length, key)
+    xor_enc_dec(data, data, length, secretKey)
     with open(outputfile, 'wb') as f:
         f.write(data)
